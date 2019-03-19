4a9fa9f7dde190278376f1de78f3c6c2bb460f18
albertgoncalves <goncalves.albert@gmail.com>
2019-03-19 09:20:47 -0400

    rearranged

--- shell.nix
+++ shell.nix
@@ -1,17 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs; mkShell {
     name = "Python";
-    buildInputs = [ (python36.withPackages(ps: with ps;
-                        [ slackclient
-                          statsmodels
-                          numpy
-                          pytz
-                          flake8
-                          pytest
-                          mypy
-                        ]
-                    ))
-                  ];
+    buildInputs = [
+        (python36.withPackages(ps: with ps; [
+            flake8
+            pytest
+            mypy
+            numpy
+            pytz
+            slackclient
+            statsmodels
+        ]))
+    ];
     shellHook = ''
         . .alias
     '';
