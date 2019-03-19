80de2135ada9b4f1193fd2104d4b65fbe95296a5
albertgoncalves <goncalves.albert@gmail.com>
2019-03-19 08:31:14 -0400

    small moves

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 from os import environ
 from pprint import pprint
 from time import sleep
-from typing import Iterator, Optional, Tuple
+from typing import Any, Iterator, Optional, Tuple
 
 from slackclient import SlackClient  # type: ignore
 
@@ -12,25 +12,30 @@ from src.parse import parse
 from src.response import response
 from src.utils import newlines
 
+# Currently lacking typedefs for the slackclient module; while this alias will
+# not throw any helpful compile-time warnings or errors, it does provide a
+# minimal amount of type-level information to the reader.
+SLACK_CLIENT = Any
 
-def send( slack_client
+
+def send( slack_client: SLACK_CLIENT
         , bot_name: Optional[str]
         , command: Optional[str]
         , channel: Optional[str]
-        ):
-    sleep(0.1)
+        ) -> SLACK_CLIENT:
     return slack_client.api_call( "chat.postMessage"
                                 , channel=channel
                                 , text=response(command, bot_name)
                                 )
 
 
-def loop( slack_client
+def loop( slack_client: SLACK_CLIENT
         , bot_name: Optional[str]
         , commands: Iterator[Tuple[Optional[str], Optional[str]]]
         ) -> None:
     for command in commands:
         pprint(send(slack_client, bot_name, *command))
+        sleep(0.15)
 
 
 def death(bot_name: Optional[str]) -> str:
@@ -59,7 +64,7 @@ def main() -> None:
                     , bot_name
                     , parse(bot_id, slack_client.rtm_read())
                     )
-                sleep(1)
+                sleep(0.5)
         else:
             print("Hmm, unable to connect.")
     except KeyboardInterrupt:
