6ce6c039b48733db03d5e61ae493fb738f537ed3
albertgoncalves <goncalves.albert@gmail.com>
2019-03-19 22:55:50 -0400

    memo-ize-ing

--- main.sh
+++ main.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 
+set -e
+
 main () {
     set -e
     for f in $(find . -type f -name "*.py"); do
-        echo "linting $f"
-        lint $f
+        if (( $time < $(stat -c %Y $f) )); then
+            echo "linting $f"
+            lint $f
+        fi
     done
-    pytest
+    pytest src/test/
     python_with .env src/main.py
 }
 
 export -f main
+timestamp=".time"
+
+if [ ! -e $timestamp ]; then
+    time=0
+else
+    time=$(cat $timestamp)
+fi
 
 if [ -z $NIX_SHELL ]; then
     nix-shell --run main
 else
     main
 fi
+
+echo $(date +%s) > $timestamp
--- shell.nix
+++ shell.nix
@@ -11,6 +11,7 @@ with pkgs; mkShell {
             slackclient
             statsmodels
         ]))
+        coreutils
     ];
     shellHook = ''
         export NIX_SHELL=1
