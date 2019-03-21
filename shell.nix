371526e5e91a5129410983e70979ce10cae5213b
albertgoncalves <goncalves.albert@gmail.com>
2019-03-20 21:19:42 -0400

    rearranging

--- shell.nix
+++ shell.nix
@@ -3,13 +3,13 @@ with pkgs; mkShell {
     name = "Python";
     buildInputs = [
         (python36.withPackages(ps: with ps; [
-            flake8
-            pytest
-            mypy
-            numpy
-            pytz
             slackclient
             statsmodels
+            numpy
+            pytz
+            pytest
+            mypy
+            flake8
         ]))
         coreutils
     ];
