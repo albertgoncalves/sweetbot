11b3221e2188e5cef1066a8d3d69cbefe10d53a0
albertgoncalves <goncalves.albert@gmail.com>
2019-03-21 21:37:49 -0400

    forgot these

---
+++ src/stubs/scipy/stats.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from typing import List, Tuple
+
+
+def linregress( x: List[float]
+              , y: List[float]
+              ) -> Tuple[float, float, float, float, float]:
+    ...
